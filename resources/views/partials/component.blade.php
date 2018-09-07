360322c76a4ee3d12605e50c6c83b8abbabc4e85
Graeme Fawcett <graeme.fawcett@proquest.com>
2018-09-07 10:48:55 -0400

    DVOPS-10040: remove popup icon

--- resources/views/partials/component.blade.php
+++ resources/views/partials/component.blade.php
@@ -1,25 +1,13 @@
 <li class="list-group-item {{ $component->group_id ? "sub-component" : "component" }}">
-    <div class="row">
-      @if($component->link)
-      <div class="col-xs-2">
-        {{ $component->name }}
-      </div>
-      <div class="col-xs-1">
-        <a href="{{ $component->link }}" class="btn btn-link" target="_blank">
-          <i class="uxf-icon uxf-popup">
-          </i>
-        </a>
-      </div>
-      @else
-      <div class="col-xs-3">
-        {{ $component->name }}
-      </div>
-      @endif
+    @if($component->link)
+    <a href="{{ $component->link }}"  target="_blank">
+      {{ $component->name }}
+    </a>
+    @else
+    {{ $component->name }}
+    @endif
 
-    <div class="col-xs-9">
-      <span class="pull-right">
-        <small class="text-component-{{ $component->status }} {{ $component->status_color }}" data-toggle="tooltip" title="{{ trans('cachet.components.last_updated', ['timestamp' => $component->updated_at_formatted]) }}">{{ $component->human_status }}</small>
-      </span>
-    </div>
-    </div>
+    <span class="pull-right">
+      <small class="text-component-{{ $component->status }} {{ $component->status_color }}" data-toggle="tooltip" title="{{ trans('cachet.components.last_updated', ['timestamp' => $component->updated_at_formatted]) }}">{{ $component->human_status }}</small>
+    </span>
 </li>
